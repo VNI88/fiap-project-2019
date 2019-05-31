35ba3e467da1d42dbb4001261d368189383fee76
Vinicius Leite <vini_cavalcanteee@hotmail.com>
2019-05-31 13:24:17 -0300

    First commit

---
+++ medicinow-sanofi/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div >
+    <TopMenu/>
+  </div>
+</template>
+
+<script>
+import TopMenu from './components/TopMenu.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TopMenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ medicinow-sanofi/src/components/TopMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand href="#">MediciNow</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+        
+          <b-navbar-nav>
+            <b-nav-item href="#">Acesse sua conta</b-nav-item>
+            <b-nav-item href="#">Registre-se</b-nav-item>
+          </b-navbar-nav>
+          
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopMenu',
+  props: {
+  }
+}
+</script>
+
+
