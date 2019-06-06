932b02bf8ffbd62538b24cbb5fd9554566a23028
Vinicius Leite <vini_cavalcanteee@hotmail.com>
2019-06-06 01:24:16 -0300

    New Login Page

--- medicinow-sanofi/src/components/LoginPage.vue
+++ medicinow-sanofi/src/components/LoginPage.vue
@@ -1,57 +1,51 @@
 <template>
   <div class="loginpage">
+  <b-row  >
+    <b-col height="100%" align="center">
+      <b-img height="200em"  :src="require('../assets/bandaids.png')"  alt="Responsive image" />
+    <h1 class='txt'>MediciNow</h1>
+  </b-col>
+  <b-col height="100%">
+    <b-form >
+      <b-form-group
+        id="input-group-1"
+        label="Nome:"
+        label-for="input-1"
+        label-size="lg"
+        label-class="font-weight-bold pt-0"
+      >
+      <b-form-input
+       id="input-1"
+       v-model="form.name"
+       required
+       placeholder="Primeiro nome"
+       size="lg"
+      ></b-form-input>
+      </b-form-group>
 
-    <div >
-      <b-row >
-        <b-column>
-          <b-card  class="size">
-            <h1 class="logo-text">
-              <b-img class="logo"  height="100" :src="require('../assets/bandaids.png')"  alt="Responsive image" />MediciNow</h1>
-            </b-card>
-          </b-column>
-          <b-column>
-            <b-card class="size">
-              <b-form>
-                <b-form-group
-                id="input-group-5"
-                label="Email:"
-                label-for="input-5"
-                label-size="lg"
-                label-class="font-weight-bold pt-0"
-                >
-                <b-form-input
-                id="input-5"
-                v-model="form.email"
-                type="email"
-                required
-                placeholder="Insira o email"
-                ></b-form-input>
-                </b-form-group>
+      <b-form-group
+        id="input-group-5"
+        label="E-mail:"
+        label-for="input-5"
+        label-size="lg"
+        label-class="font-weight-bold pt-0"
+      >
+      <b-form-input
+        id="input-5"
+        v-model="form.email"
+        type="email"
+        required
+        placeholder="Insira o e-mail"
+        size="lg"
+      ></b-form-input>
+      </b-form-group>
+    </b-form >
+    <div text-align="center">
+     <b-button variant="outline-dark" size="lg" >Login</b-button>
+   </div>
 
-                <b-form-group
-                  id="input-group-6"
-                  label="Senha:"
-                  label-size="lg"
-                  label-class="font-weight-bold pt-0"
-                  label-for="input-6"
-                >
-                <b-form-input
-                  id="input-6"
-                  v-model="form.password"
-                  type="password"
-                  required
-                  placeholder="Insira a senha"
-                  aria-describedby="password-help-block"
-                ></b-form-input>
-                <b-form-text id="password-help-block">
-                  Sua senha deve conter de 8-20 caracteres, com letras e números.
-                </b-form-text>
-                </b-form-group>
-              </b-form>
-            </b-card>
-          </b-column>
-        </b-row>
-      </div>
+  </b-col>
+  </b-row>
     </div>
   </template>
 
@@ -77,13 +71,13 @@
 .logo {
   margin:10px;
 }
-.logo-text {
+.txt {
   font-weight:bold;
   text-align: center;
 }
 .size {
-  height: 500px;
-  width:500px;
-  margin: 200px 0px 200px 0px;
+  display: block;
+     width: 100%;
 }
+
 </style>
