ab9df5a9c11d650cd99c6b52fc5c3fbb6afbf672
Vinicius Leite <vini_cavalcanteee@hotmail.com>
2019-06-06 14:34:02 -0300

    New basic login screen

--- medicinow-sanofi/src/components/LoginPage.vue
+++ medicinow-sanofi/src/components/LoginPage.vue
@@ -1,93 +1,99 @@
 <template>
-  <div class="loginpage">
+<div class="loginpage" >
+  <div class="mod">
+    <h1 class='txt'>
+      <b-img   height="100px":src="require('../assets/bandaids.png')"  alt="Responsive image" />
+      MediciNow</h1>
+  </div>
 
-  <div class="size">
-  <b-row>
-    <b-col height="100%" align="center" >
-      <b-img  :src="require('../assets/bandaids.png')"  alt="Responsive image" />
-    <h1 class='txt'>MediciNow</h1>
-
-  </b-col>
-
-  <b-col height="100%">
-    <b-form >
-      <b-form-group
+  <div class="colored-div">
+    <b-card class="size" border-variant="dark" >
+      <b-form >
+        <b-form-group
         id="input-group-5"
         label="E-mail:"
         label-for="input-5"
         label-size="lg"
         label-class="font-weight-bold pt-0"
-      >
-      <b-form-input
+        >
+        <b-form-input
         id="input-5"
         v-model="form.email"
         type="email"
         required
         placeholder="Insira o e-mail"
         size="lg"
-      ></b-form-input>
+        ></b-form-input>
       </b-form-group>
 
       <b-form-group
-        id="input-group-6"
-        label="Senha:"
-        label-for="input-6"
-        label-size="lg"
-        label-class="font-weight-bold pt-0"
+      id="input-group-6"
+      label="Senha:"
+      label-for="input-6"
+      label-size="lg"
+      label-class="font-weight-bold pt-0"
       >
       <b-form-input
-        id="input-6"
-        v-model="form.password"
-        size="lg"
-        type="password"
-        required
-        placeholder="Insira a senha"
+      id="input-6"
+      v-model="form.password"
+      size="lg"
+      type="password"
+      required
+      placeholder="Insira a senha"
       ></b-form-input>
-      </b-form-group>
-    </b-form >
-    <div class="text-center">
-     <b-button variant="outline-dark" size="lg">Login</b-button>
-     <p class="note">Esqueceu sua senha?
-       <a href=" ">Clique aqui</a>
-     </p>
-   </div>
+    </b-form-group>
+  </b-form >
+  <div class="text-center">
+    <b-button variant="outline-dark" size="lg">Login</b-button>
+    <p class="note">Esqueceu sua senha?
+      <a href=" ">Clique aqui</a>
+    </p>
+  </div>
+</b-card>
 
-  </b-col>
-  </b-row>
-    </div>
-    </div>
-  </template>
+
+
+</b-row>
+</div>
+</div>
+</template>
 
 <script>
-  export default {
-    name: 'loginpage',
-    data() {
-    return {
-      form: {
-        name: '',
-        email: '',
-        food: null,
-        checked: []
-      },
-      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-      show: true
-    }
-  },
+export default {
+name: 'loginpage',
+data() {
+  return {
+    form: {
+      name: '',
+      email: '',
+      food: null,
+      checked: []
+    },
+    foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+    show: true
   }
+},
+}
 </script>
 
 <style>
 .note {
-   margin-top: 20px;
+margin-top: 20px;
 }
 .txt {
-  font-weight:bold;
-  text-align: center;
+font-weight:bold;
+text-align: center;
+}
+.colored-div{
+background-color: ;
 }
 .size {
-  margin: 200px 0px 200px 0px;
-  height:  100%;
+margin: 40px auto 40px auto ;
+width:450px ;
 
 }
-
+.mod{
+margin-top: 20px;
+align-content:center;
+}
 </style>
