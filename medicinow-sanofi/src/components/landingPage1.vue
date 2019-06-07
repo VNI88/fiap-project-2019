e9625e08ca3dbe3dd6de24d0d1e518eeed6f016a
Vinicius Leite <vini_cavalcanteee@hotmail.com>
2019-06-07 10:30:47 -0300

    New Images and navbar fixed positioning

--- medicinow-sanofi/src/components/landingPage1.vue
+++ medicinow-sanofi/src/components/landingPage1.vue
@@ -1,46 +1,52 @@
 <template>
   <div class="home">
     <div>
-      <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar toggleable="lg" type="light" variant="light" fixed="top">
         <b-navbar-brand href="#">
           <b-img height="35" :src="require('../assets/bandaids.png')"  alt="Responsive image" />
           MediciNow
         </b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="ml-auto" >
             <b-nav-item  href="#"> Sobre nós</b-nav-item>
-            <b-nav-item  href="#">Plataforma</b-nav-item>
+            <b-nav-item  href="#plataform">Plataforma</b-nav-item>
             <b-nav-item  href="#">Contato</b-nav-item>
           </b-navbar-nav>
-
-
         </b-collapse>
       </b-navbar>
     </div>
-    <div>
 
+    <div class="presentation">
+      <h1 class="presentation-text">A melhor solução do mercado para agendamento de consultas.</h1>
+      <div class='buttons-container'>
+        <b-row>
+          <b-col lg="4" class="pb-2">
+            <b-button href="/#/register " variant="outline-dark" size="lg">Registre-se</b-button>
+          </b-col>
+          <b-col lg="4" class="pb-2">
+            <b-button class="ml-auto" href="/#/login" variant="outline-primary" size="lg">Acesse</b-button>
+          </b-col>
+          <b-col lg="4" class="pb-2">
+            <b-button variant="outline-dark" size="lg">Baixe o aplicativo</b-button>
+          </b-col>
+        </b-row>
+      </div>
+      <div class="plataform-explanation" id="plataform">
+        <b-row>
+          <b-col>
+            <b-img width="500px"height="350px" :src="require('../assets/plataform_diagram.png')"  alt="Responsive image" />
+          </b-col>
+          <b-col>
+            <p class="plataform-text">A MediciNow integra mobilidade, praticidade e organização em uma plataforma web e mobile. Marque suas consultas, envie seus exames, veja seu calendário tudo em um único lugar!</p>
+          </b-col>
+        </b-row>
+      </div>
+    </div>
 
-        <h1 class="presentation-text">A melhor solução do mercado para agendamento de consultas.</h1>
-        <div class='buttons-container'>
-          <b-row>
-            <b-col lg="4" class="pb-2">
-              <b-button href="/#/register " variant="outline-dark" size="lg">Registre-se</b-button>
-            </b-col>
-            <b-col lg="4" class="pb-2">
-              <b-button class="ml-auto" href="/#/login" variant="outline-primary" size="lg">Acesse</b-button>
-            </b-col>
-            <b-col lg="4" class="pb-2">
-              <b-button variant="outline-dark" size="lg">Baixe o aplicativo</b-button>
-            </b-col>
-          </b-row>
-        </div>
 
   </div>
-
-</div>
 </template>
 
 <script>
@@ -59,8 +65,20 @@ export default {
   margin: 100px;
   font-size:4em;
 }
+.presentation{
+  height: 400px;
+
+}
 .buttons-container{
   text-align: center;
-  margin: 0 300px;
+  margin: 0 300px 300px 300px;
+}
+.plataform-explanation{
+  margin: 0px 50px 100px 0px;
+}
+.plataform-text{
+  margin-top: 50px;
+  font-size: 2em;
+  text-align:center;
 }
 </style>
---
+++ medicinow-sanofi/teste.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="loginpage">
+
+
+  <div class="size">
+  <b-row background-color="black">
+    <div class="mod">
+    <b-col height="100%" align="center" >
+      <b-img  :src="require('../assets/bandaids.png')"  alt="Responsive image" />
+    <h1 class='txt'>MediciNow</h1>
+
+  </b-col>
+</div>
+  <b-col height="100%" >
+    <b-form >
+      <b-form-group
+        id="input-group-5"
+        label="E-mail:"
+        label-for="input-5"
+        label-size="lg"
+        label-class="font-weight-bold pt-0"
+      >
+      <b-form-input
+        id="input-5"
+        v-model="form.email"
+        type="email"
+        required
+        placeholder="Insira o e-mail"
+        size="lg"
+      ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-6"
+        label="Senha:"
+        label-for="input-6"
+        label-size="lg"
+        label-class="font-weight-bold pt-0"
+      >
+      <b-form-input
+        id="input-6"
+        v-model="form.password"
+        size="lg"
+        type="password"
+        required
+        placeholder="Insira a senha"
+      ></b-form-input>
+      </b-form-group>
+    </b-form >
+    <div class="text-center">
+     <b-button variant="outline-dark" size="lg">Login</b-button>
+     <p class="note">Esqueceu sua senha?
+       <a href=" ">Clique aqui</a>
+     </p>
+   </div>
+
+  </b-col>
+  </b-row>
+    </div>
+    </div>
+  </template>
+
+<script>
+  export default {
+    name: 'loginpage',
+    data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        food: null,
+        checked: []
+      },
+      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+      show: true
+    }
+  },
+  }
+</script>
+
+<style>
+.note {
+   margin-top: 20px;
+}
+.txt {
+  font-weight:bold;
+  text-align: center;
+}
+.size {
+  margin: 200px 0px 0px 0px;
+  height:  100%;
+}
+.mod{
+   width: 500px;
+}
+</style>
