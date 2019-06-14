455a5080d780b237e937cfc0f8251c3771f2522d
Vinicius Leite <vini_cavalcanteee@hotmail.com>
2019-06-14 10:55:04 -0300

    Updates after validation

--- medicinow-sanofi/src/components/landingPage1.vue
+++ medicinow-sanofi/src/components/landingPage1.vue
@@ -10,9 +10,10 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="ml-auto" >
-            <b-nav-item  href="#"> Sobre nós</b-nav-item>
+            <b-nav-item  href="#about_us"> Sobre nós</b-nav-item>
             <b-nav-item  href="#plataform">Plataforma</b-nav-item>
-            <b-nav-item  href="#contact">Contato</b-nav-item>
+            <b-nav-item  href="#contact-1">Contato</b-nav-item>
+            <b-nav-item   href="/#/register"><b-button  variant="outline-primary" size="sm">Registre-se</b-button></b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
@@ -21,94 +22,68 @@
     <div class="presentation">
       <h1 class="presentation-text">A melhor solução do mercado para agendamento de consultas.
         <div class='buttons-container'>
-          <b-row >
-            <b-col lg="4" class="pb-2">
-              <b-button href="/#/register " variant="outline-dark" size="lg">Registre-se</b-button>
-            </b-col>
-            <b-col lg="4" class="pb-2">
-              <b-button class="ml-auto" href="/#/login" variant="outline-primary" size="lg">Acesse</b-button>
-            </b-col>
-            <b-col lg="4" class="pb-2">
-              <b-button variant="outline-dark" size="lg">Baixe o aplicativo</b-button>
-            </b-col>
-          </b-row>
+          <b-button class="ml-auto" href="/#/login" variant="outline-primary" size="lg">Acesse</b-button>
         </div>
       </h1>
-  </div>
-  <div>
-    <b-row>
-      <b-col >
-        <b-card >
-          <b-img  height="150px" :src="require('../assets/Doctor.png')"  alt="Responsive image" />
-        </b-card>
-      </b-col>
-      <b-col>
-      </b-col>
-              <transition name="custom-classes-transition" enter-active-class="animated tada" leave-active-class="animated bounceOutRight" >
-      <b-col >
-        <b-img   :src="require('../assets/bandaids.png')"  alt="Responsive image" />
+    </div>
 
-      </b-col>
-    </transition>
-      <b-col>
-      </b-col>
-      <b-col>
-        <b-card >
-          <b-img height="150px"   :src="require('../assets/people.png')"  alt="Responsive image" />
-        </b-card>
-      </b-col>
-    </b-row>
-  </div>
-  <div class="plataform-explanation" id="plataform">
-    <h1 class="plataform-title">Plataforma</h1>
-    <b-row>
-      <b-col>
-        <b-card v-b-toggle.collapse-1 align="center" >
-          <b-card-text class="plataform-card-title">
-            Web
-          </b-card-text>
-          <b-img   :src="require('../assets/www-logo.png')"  alt="Responsive image" />
-          <b-collapse id="collapse-1" class="mt-2">
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-          </b-collapse>
-        </b-card>
-      </b-col>
-      <b-col>
-        <b-card  v-b-toggle.collapse-2 align="center">
-          <b-card-text class="plataform-card-title" >
-            Mobile
-          </b-card-text>
-          <b-img   :src="require('../assets/mobile.png')"  alt="Responsive image" />
-          <b-collapse id="collapse-2" class="mt-2">
-            <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-          </b-collapse>
-        </b-card>
-      </b-col>
-    </b-row>
-  </div>
+    <div id="about_us" >
+      <b-card align="center">
+        <b-img :src="require('../assets/About-us.png')"  alt="Responsive image" />
+      </b-card>
+      <p class="about-us-text">Fundada em 2019, a MediciNow veio com a missão de desburocratizar o agendamento e organização de consultas médicas, com foco em uma solução Web e Mobile.</p>
+    </div>
 
-  <div class="contact" id="contact-1">
-    <b-card-group >
-      <b-card title="Dúvidas?" sub-title="Deixe sua mensagem abaixo, lhe responderemos o mais rápido possível.">
-        <b-form>
+    <div class="plataform-explanation" id="plataform">
+      <h1 class="plataform-title">Plataforma</h1>
+      <b-row>
+        <b-col>
+          <b-card v-b-toggle.collapse-1 align="center" >
+            <b-card-text class="plataform-card-title">
+              Web
+            </b-card-text>
+            <b-img   :src="require('../assets/www-logo.png')"  alt="Responsive image" />
+            <b-collapse id="collapse-1" class="mt-2">
+              <b-card-text>
+                Acesse ao sistema de qualquer navegador. Veja dashboards customizadas, atualize suas informações e muito mais.
+              </b-card-text>
+            </b-collapse>
+          </b-card>
+        </b-col>
+        <b-col>
+          <b-card  v-b-toggle.collapse-2 align="center">
+            <b-card-text class="plataform-card-title" >
+              Mobile
+            </b-card-text>
+            <b-img   :src="require('../assets/mobile.png')"  alt="Responsive image" />
+            <b-collapse id="collapse-2" class="mt-2">
+              <b-card-text>
+                Carregue toda a informação que precisa na palma de suas mãos!
+              </b-card-text>
+            </b-collapse>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
+
+    <div class="contact" id="contact-1">
+      <b-card-group >
+        <b-card title="Dúvidas?" sub-title="Deixe sua mensagem abaixo, lhe responderemos o mais rápido possível.">
+          <b-form>
+            <b-form-group
+            id="input-group-1"
+            label="Nome:"
+            label-for="input-1"
+            >
+            <b-form-input
+            id="input-1"
+            v-model="form.name"
+            required
+            placeholder="Digite aqui seu nome"
+            ></b-form-input>
+          </b-form-group>
+
           <b-form-group
-          id="input-group-1"
-          label="Nome:"
-          label-for="input-1"
-          >
-          <b-form-input
-          id="input-1"
-          v-model="form.name"
-          required
-          placeholder="Digite aqui seu nome"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
           id="input-group-5"
           label="Email:"
           label-for="input-5"
@@ -130,6 +105,9 @@
         max-rows="6"
         ></b-form-textarea>
       </b-form>
+      <div class="contact-button">
+        <b-button  variant="outline-dark" size="sm">Enviar</b-button>
+      </div>
     </b-card>
     <b-card title="Contatos">
       <b-card-text>
@@ -137,20 +115,23 @@
         Telefone: (11) 99843-6963
       </b-card-text>
       <b-row align="center" >
-      <b-col lg="4">
-      <a href="https://www.linkedin.com"><b-img height="35" :src="require('../assets/square-linkedin-128.png')"  alt="Responsive image" /></a>
-    </b-col>
-    <b-col lg="4">
-      <b-img height="35" :src="require('../assets/social-facebook-square2-128.png')"  alt="Responsive image" />
-    </b-col>
-    <b-col lg="4">
-      <b-img height="35" :src="require('../assets/instagram-square-128.png')"  alt="Responsive image" />
-    </b-col>
-    </b-row>
+        <b-col lg="4">
+          <a href="https://www.linkedin.com"><b-img height="35" :src="require('../assets/square-linkedin-128.png')"  alt="Responsive image" /></a>
+        </b-col>
+        <b-col lg="4">
+          <b-img height="35" :src="require('../assets/social-facebook-square2-128.png')"  alt="Responsive image" />
+        </b-col>
+        <b-col lg="4">
+          <b-img height="35" :src="require('../assets/instagram-square-128.png')"  alt="Responsive image" />
+        </b-col>
+      </b-row>
     </b-card>
-  </b-card-group>
-</div>
-
+    </b-card-group>
+  </div>
+  <div class="copyright-area">
+    <p> Copyright (c) 2019 MediciNow<br/>
+      All Rights Reserved</p>
+  </div>
 </div>
 </template>
 
@@ -159,16 +140,16 @@
 export default {
   name: 'home',
   data() {
-  return {
-    form: {
-      name: '',
-      email: '',
-      food: null,
-      checked: []
-    },
-    foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-    show: true
-  }
+    return {
+      form: {
+        name: '',
+        email: '',
+        food: null,
+        checked: []
+      },
+      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+      show: true
+    }
   },
 
 }
@@ -180,10 +161,20 @@ export default {
   padding: 200px 20px 200px 20px;
   font-size:4em;
 }
+.about-us-text {
+  text-align: center;
+  padding: 100px 100px 100px 100px;
+  font-size:2em;
+}
 .plataform-title{
   font-size: 3em;
   text-align: center;
   padding-bottom: 10px;
+}
+.about-us-title{
+  font-size: 2em;
+  text-align: center;
+  padding-bottom: 2px;
 }
 .plataform-card-title{
   font-weight: bold;
@@ -207,5 +198,15 @@ export default {
   margin-top: 50px;
   font-size: 2em;
   text-align:center;
+}
+.arrow-container{
+  align-content: center;
+}
+.contact-button{
+  text-align: center;
+  padding-top: 10px;
+}
+.copyright-area{
+  text-align: center;
 }
 </style>
