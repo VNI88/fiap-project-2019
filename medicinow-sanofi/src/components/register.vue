48130afba0a7a83f9cf07fa0a4ddcb25a91647f4
Vinicius Cavalcante Leite <vini_cavalcanteee@hotmail.com>
2019-08-20 11:51:18 -0300

    Form data communicating with api

--- medicinow-sanofi/src/components/register.vue
+++ medicinow-sanofi/src/components/register.vue
@@ -3,15 +3,15 @@
     <p class="header">MediciNOW</p>
     <div class="container ">
       <div class="notification">
-        <form >
+        <form>
 
           <b-field
             label="Nome"
-            :type="{'is-danger': errors.has('firstname')}"
-            :message="{'Por favor insira um Nome válido' : errors.firstByRule('firstname', 'required')}">
+            :type="{'is-danger': errors.has('first_name')}"
+            :message="{'Por favor insira um Nome válido' : errors.firstByRule('first_name', 'required')}">
               <b-input
-                v-model="firstname"
-                name="firstname"
+                v-model="first_name"
+                name="first_name"
                 v-validate="'required'"
                 size="is-medium"
                 placeholder="Insira o seu nome aqui">
@@ -20,11 +20,11 @@
 
           <b-field
             label="Sobrenome"
-            :type="{'is-danger': errors.has('lastname')}"
-            :message="{'Por favor insira um Sobrenome válido' : errors.firstByRule('lastname', 'required')}">
+            :type="{'is-danger': errors.has('last_name')}"
+            :message="{'Por favor insira um Sobrenome válido' : errors.firstByRule('last_name', 'required')}">
               <b-input
-                v-model="lastname"
-                name="lastname"
+                v-model="last_name"
+                name="last_name"
                 v-validate="'required'"
                 size="is-medium"
                 placeholder="Insira o seu sobrenome aqui">
@@ -89,6 +89,9 @@
 
               <b-field label="CRM">
                 <b-input
+                v-model="crm"
+                name="crm"
+
                 size="is-medium"
                 placeholder="Insira o seu CRM"></b-input>
               </b-field>
@@ -103,8 +106,6 @@
                 Eu concordo com os termos e condições de uso
               </b-checkbox>
           </b-field>
-
-
         </form>
       </div>
       <b-field grouped position="is-centered">
@@ -113,13 +114,8 @@
       <div>
         <p>{{pacient}}</p>
       </div>
-  </div>
-
+    </div>
   </section>
-
-
-
-
 </template>
 
 <script>
@@ -134,33 +130,17 @@ Vue.use(VeeValidate, {
 export default {
   data() {
     return {
-      firstname: null,
-      lastname: null,
+      last_name: null,
+      first_name: null,
+      telephone: null,
       email: null,
       password: null,
       flagTerms: false,
-      pacient:{}
+      pacient: null,
+      crm: null
     }
   },
-  /*
-  data() {
-    return {
-      medicalAgreements: {},
-    }
-  },
-  */
   methods: {
-  /*  mounted() {
-      axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-        .then((response) =>{
-        console.log(response.data);
-        this.medicalAgreements = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    },
- */
     validateBeforeSubmit() {
       this.$validator.validateAll().then((result) => {
         if (result) {
@@ -169,27 +149,67 @@ export default {
             type: 'is-success',
             position: 'is-bottom'
           })
-
-          axios.post('https://obscure-woodland-55617.herokuapp.com/api/v1/pacients')
-            .then((response) =>{
-            console.log(response.data);
-            this.pacient = response.data;
+          console.log(this.crm);
+          if (this.crm){
+            const proxyurl = "https://cors-anywhere.herokuapp.com/";
+            axios.post( proxyurl+'https://obscure-woodland-55617.herokuapp.com/api/v1/doctors', {
+              first_name: this.first_name,
+              last_name: this.last_name,
+              telephone: this.telephone,
+              email: this.email,
+              password: this.password,
+              crm: this.crm
+          },{
+            headers: {
+              'Access-Control-Allow-Credentials' : true,
+              'Access-Control-Allow-Methods':'*',
+              'Access-Control-Allow-Headers':'*',
+              }
           })
-          .catch((error) => {
-            console.log(error);
-          });
+              .then((response) =>{
+              console.log(response);
+              return this.pacient = response.data
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          }
+          else{
+            const proxyurl = "https://cors-anywhere.herokuapp.com/";
+            axios.post( proxyurl+'https://obscure-woodland-55617.herokuapp.com/api/v1/pacients', {
+              first_name: this.first_name,
+              last_name: this.last_name,
+              telephone: this.telephone,
+              email: this.email,
+              password: this.password
 
-          return;
+          },{
+            headers: {
+              'Access-Control-Allow-Credentials' : true,
+              'Access-Control-Allow-Methods':'*',
+              'Access-Control-Allow-Headers':'*',
+              }
+          })
+              .then((response) =>{
+              console.log(response);
+              return this.pacient = response.data
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          }
+
         }
-        this.$buefy.toast.open({
-          message: 'O formulário não é válido!Por favor verifique os campos.',
-          type: 'is-danger',
-          position: 'is-bottom'
-        })
+        else {
+          this.$buefy.toast.open({
+            message: 'O formulário não é válido!Por favor verifique os campos.',
+            type: 'is-danger',
+            position: 'is-bottom'
+          })
+        }
       });
-    }
+    },
   }
-
 }
 </script>
 
