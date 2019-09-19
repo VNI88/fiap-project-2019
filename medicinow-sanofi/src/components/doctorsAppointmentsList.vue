5161fd6c510128a14877745cbeec575399711c3b
VInicius Cavalcante Leite <vini_cavalcanteee@hotmail.com>
2019-09-19 01:29:22 -0300

    New flows

--- medicinow-sanofi/src/components/doctorsAppointmentsList.vue
+++ medicinow-sanofi/src/components/doctorsAppointmentsList.vue
@@ -50,31 +50,45 @@
       </template>
     </b-navbar>
 
-    <div v-for="appointment in appointments" class="modal-card" style=" margin-top: 40px; width: auto; border-style: solid; border-width: 2px; border-radius: 10px; border-color: blue; ">
-      <p style="fontWeight: bold;">Data</p><br>
-        {{appointment.appointment_day}}
-      <p style="fontWeight: bold;">Hora</p>
-        {{appointment.appointment_hour}}
-      <p style="fontWeight: bold;">Paciente</p>
-        {{appointment.pacient_first_name}}  {{appointment.pacient_last_name}}
-      <p style="fontWeight: bold;">Convênio</p>
-        {{appointment.m.brand}} {{appointment.m.plan}}
-      <p style="fontWeight: bold;">Consultório</p>
-        {{appointment.street_address}}
-      <b-button size="is-medium" type="is-danger" v-on:click="cancelAppointment(appointment)">Cancelar Consulta</b-button>
+    <div v-if="zeroAppointments === 0" style ="fontWeight: bold; text-align: center; fontSize: 40px; padding: 100px;">
+      <p >Você não possui consultas marcadas para hoje.</p>
+    </div>
+    <b-collapse
+        :open="false"
+        aria-id="contentIdForA11y1">
+    <div  v-for="appointment in appointments" class="modal-card" role="button" style=" margin-top: 40px; width: auto; border-style: solid; border-width: 1px; border-radius: 10px; border-color: blue; height: auto; margin-left: 15px;" aria-controls="contentIdForA11y1" slot="trigger">
+
+      <div style="padding: 20px;">
+        <p style="fontWeight: bold;">Data</p>
+          {{appointment_day}}
+        <p style="fontWeight: bold;">Hora</p>
+          {{appointment.appointment_hour}}
+        <p style="fontWeight: bold;">Paciente</p>
+          {{appointment.pacient_first_name}}  {{appointment.pacient_last_name}}
+        <p style="fontWeight: bold;">Convênio</p>
+          {{appointment.brand}} {{appointment.plan}}
+        <p style="fontWeight: bold;">Consultório</p>
+          {{appointment.street_address}}
+
+      </div>
+      <b-field grouped position="is-right" style="margin: 20px; ">
+        <b-button size="is-medium" type="is-danger" v-on:click="cancelAppointment(appointment.appointment_id)">Cancelar Consulta</b-button>
+      </b-field>
+
       <b-modal :active.sync="isCardModalActive" :width="640" scroll="keep" >
         <div class="card" style="border-radius: 10px; ">
             <div class="card-content">
               <div class="media">
                   <div class="media-content" style="text-align: center;">
                     <img src="../assets/green_check.png" alt="Image" style="height:100px;">
-                    <p class="title is-4">Sua consulta foi cancelada com sucesso!</p>
+                    <p class="title is-4">A consulta foi cancelada com sucesso!</p>
                   </div>
               </div>
             </div>
         </div>
       </b-modal>
     </div>
+    </b-collapse>
   </section>
 </template>
 
@@ -104,7 +118,7 @@ export default {
       visible: false,
       isFullPage:true,
       isCardModalActive: false,
-      appointment_day: `${date.getDate(this.date)}-${date.getMonth(this.date)}-${date.getFullYear(this.date)}`,
+      appointment_day: `${dateFormatter.getFullYear(this.date)}-${dateFormatter.getMonth(this.date)+1}-${dateFormatter.getDate(this.date)}`,
       appointment_hour:  `${dateFormatter.getHours(this.hour)}:${dateFormatter.getMinutes(this.hour)}`,
       appointment: null,
       appointments: null,
@@ -112,18 +126,17 @@ export default {
       pacient_name: [],
       street_address: [],
       office_id: null,
+      zeroAppointments: null,
       doctor_id: this.$session.get('doctor_id'),
       token: this.$session.get('token')
     }
   },
 
-
+//axios.get( proxyurl+`https://mednow.herokuapp.com/api/v1/appointments/doctor_day_list/${this.appointment_day}/${this.doctor_id}`,
   mounted() {
 
     this.submitedName = this.$session.get('userName'),
-    console.log(this.token)
-    console.log(this.doctor_id)
-    axios.get( proxyurl+`https://mednow.herokuapp.com/api/v1/appointments/${this.appointment_day}/${this.doctor_id}`, {
+    axios.get( proxyurl+`https://mednow.herokuapp.com/api/v1/appointments/doctor_day_list/2019-09-18/${this.doctor_id}`, {
        headers: {
          'Access-Control-Allow-Credentials' : true,
          'Access-Control-Allow-Methods':'*',
@@ -135,7 +148,7 @@ export default {
        console.log(response);
 
        const loadingComponent = this.$buefy.loading.open({
-           container: this.isFullPage ? null : this.$refs.element.$el
+
        })
        setTimeout(() => loadingComponent.close(), 3 * 1000)
 
@@ -143,6 +156,10 @@ export default {
       })
      .catch((error) => {
        console.log(error.response);
+       if (error.response.data.error.received === 0) {
+         this.zeroAppointments = 0
+         console.log(this.zeroAppointments)
+       }
      });
 
   },
@@ -159,21 +176,20 @@ export default {
       return this.$router.push('/')
     },
 
-    cancelAppointment: function(appointment) {
-      this.appointment = appointment;
+    cancelAppointment: function(appointment_id) {
       const proxyurl = "https://cors-anywhere.herokuapp.com/";
-      axios.delete( proxyurl+`https://mednow.herokuapp.com/api/v1/appointments/${this.appointment.id}`, {
-      },{
+      axios.delete( proxyurl+`https://mednow.herokuapp.com/api/v1/appointments/${appointment_id}`,{
         headers: {
           'Access-Control-Allow-Credentials' : true,
           'Access-Control-Allow-Methods':'*',
           'Access-Control-Allow-Headers':'*',
-          'Authorization': "Bearer " + this.token
+          'Authorization': `Bearer ${this.token}`,
         }
       })
       .then((response) =>{
         console.log(response);
-         return this.isCardModalActive = true
+        this.isCardModalActive = true
+        this.$router.push('/doctor_appointments')
         }
       )
       .catch((error) => {
